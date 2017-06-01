f3cf30ae74fa4049cfbae1aeb8ed8f87956ca4cf
hackerudroids <tomerjava@gmail.com>
2017-06-01 18:20:08 +0300

    Initial commit

---
+++ app/src/main/java/hackeru/edu/parsingjson/WeatherDataSource.java
@@ -0,0 +1,10 @@
+package hackeru.edu.parsingjson;
+
+/**
+ * Get Weather Data from OpenWeatherMap API: application programming interface
+ */
+
+public class WeatherDataSource {
+    //http://api.openweathermap.org/data/2.5/weather?q=Tel-Aviv,IL&appid=288ca3c192923f79bd74f4d01a9299c0&units=metric
+
+}
